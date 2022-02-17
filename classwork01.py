21b5d126db6e0f0cfe3ba8c7b6c3db467cef8ecb
s-vakoliuk <s.vakoliuk@gmail.com>
2022-02-17 22:56:59 +0200

    17 february

--- classwork01.py
+++ classwork01.py
@@ -14,23 +14,63 @@ users_list = [] # порожній список users_list
 
 user = {} # порожня бібліотека user
 while True:
-    id = int(input("Введіть свій id: "))
-    name = str(input("Введіть своє ім'я: "))
-    age = int(input("Введіть свій вік: "))
-    status = bool(input("Введіть свій статус: "))
+    print("1) додававання нового юзера)")
+    print("2) вивід всіх юзерів")
+    print("3) вивід всіх юзерів за віком")
+    print("4) видалення юзера по id")
+    print("5) заміна статуса юзера на протилежний")
+    print("6) вихід з меню")
 
-    print("Введіть дані наступного користувача")
+    choice = input('Зробіть вибір: ')
 
-    user={'id':id,
-      'name':name,
-      'age':age,
-      'status':status,
-      }
+    if choice == '1':
+        print("Введіть дані користувача:")
+        id = int(input("Введіть свій id: "))
+        name = str(input("Введіть своє ім'я: "))
+        age = int(input("Введіть свій вік: "))
+        status = bool(input("Введіть свій статус: "))
 
-    # for user in range (3):
-    users_list.append(user)
-    if len(users_list)==4:
+        user = {'id': id,
+                'name': name,
+                'age': age,
+                'status': status,
+                }
+
+        users_list.append(user) # додати словник (користувача) user до списку users_list
+
+        repeat= input("Додати дані наступного користувача? Якщо так введіть 1, якщо ні 0: ")
+        if repeat == "1":
+            choice == "1"
+        elif repeat == "0":
+            print(users_list)
+            continue
+
+    elif choice == '2':
+        for item in users_list:
+            print(item)
+
+    elif choice == '3':
+        name = input('Введіть вік користувача: ')
+        for item in users_list:
+            if item['age'] == age:
+                print(item)
+
+
+    elif choice == '4':
+        id = input('Введіть id користувача для видалення: ')
+        for item in range(len(users_list)):
+            if item['id'] == id:
+                del users_list[item]
+
+        print(users_list)
+
+    elif choice == '5':
+        status = input('Введіть status користувача для видалення: ')
+        for item in range(len(users_list)):
+            if users_list[item] == status['true']:
+                users_list[item] = 'false'
+
+        print(users_list)
+
+    elif choice == '6':
         break
-
-
-print(users_list)
