8db4cca1e71894e8e520a1a5b50608d1bd2e8b5f
s-vakoliuk <s.vakoliuk@gmail.com>
2022-02-20 19:58:15 +0200

    20 february 2022

--- function_todo_list.py
+++ function_todo_list.py
@@ -7,30 +7,22 @@
 виведіть весь список справ"""
 
 def notebook():
-    todo_list: list[str] = []
+    todo_list = []
 
-    def add_todo(new_todo: str) -> None:
+    def add_todo(new_todo):
         nonlocal todo_list
         todo_list.append(new_todo)
 
-    def get_all() -> list[str]:
+    def get_all():
         return todo_list
 
     return {'add_todo': add_todo, 'get_all': get_all}
-    # return (get_all, add_todo)
 
 add_todo, get_all = notebook()
+
 while True:
     for item in range (5):
         add_todo = str(input("Enter the case in the to-do list: "))
     break
+
 print(get_all())
-
-
-# add_todo('wake up at 5 oclock')
-# add_todo('take a shower')
-# add_todo('make to cook breakfast')
-# add_todo('have breakfast')
-# add_todo('go to work at 7 oclock')
-
-# print(get_all())
