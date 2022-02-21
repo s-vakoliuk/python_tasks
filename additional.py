35f0a94ccf04ef5b13b151d814fa9e6378b2dc03
s-vakoliuk <s.vakoliuk@gmail.com>
2022-02-21 23:30:53 +0200

    21 february 2022

--- additional.py
+++ additional.py
@@ -1,37 +1,6 @@
 # Створити функцію котра приймає число і повертає так, як в прикладі.
 # Наприклад: ввели 3275, а виведе "3000 + 200 + 70 +5"
 
-# number = (input("Введіть число: "))
-# print("Введене число: ", number)
-#
-# print("Кількість цифр: ", len(number))
-#
-# listOfNumbers = list(number) # переведення в список рядків
-# print("Список рядків", listOfNumbers)
-#
-# for item in range(len(listOfNumbers)):
-#     listOfNumbers[item]=int(listOfNumbers[item])
-# print("Список чисел", listOfNumbers)
-#
-# result=[]
-# for index, value in enumerate(listOfNumbers):
-#     if index==0:
-#         result.append(value*1000)
-#     elif index==1:
-#         result.append(value * 100)
-#     elif index==2:
-#         result.append(value * 10)
-#     elif index==3:
-#         result.append(value * 1)
-#
-# print("Результат списку чисел", result)
-# result = [str(i) for i in result] # переведення списку чисел в список рядків
-# print("Результат списку рядків", result)
-# resultString = "+".join(result)
-# print(resultString)
-
-###############################
-
 number = (input("Введіть число: "))
 print("Введене число: ", number)
 count=len(number)
