a2d196a6eacad567acef0e22da9f4582cdca7d71
s-vakoliuk <s.vakoliuk@gmail.com>
2022-02-22 00:10:09 +0200

    21 february 2022

--- additional.py
+++ additional.py
@@ -1,31 +1,6 @@
 # Створити функцію котра приймає число і повертає так, як в прикладі.
 # Наприклад: ввели 3275, а виведе "3000 + 200 + 70 +5"
 
-# number = (input("Введіть число: "))
-# print("Введене число: ", number)
-# count=len(number)
-# print("Кількість цифр: ", count)
-#
-# listOfNumbers = list(number) # Переведення в список рядків
-# print("Список рядків", listOfNumbers)
-#
-# for item in range(len(listOfNumbers)):
-#     listOfNumbers[item]=int(listOfNumbers[item])
-# print("Список чисел", listOfNumbers)
-#
-# reverselistOfNumbers=listOfNumbers[::-1] # Формуємо зворотній список чисел
-#
-# result=[]
-# for index, value in enumerate(reverselistOfNumbers):
-#     result.append(value * (10**index))
-# print("Результат списку чисел", result)
-#
-# result[::-1] = [str(i) for i in result] # Переведення списку чисел в список рядків
-# resultString = "+".join(result)
-# print("Результат списку рядків", resultString)
-
-#########################################################
-
 def bitnumber (number):
     number=str(number)
     listOfNumbers = list(number)  # Переведення в список рядків
