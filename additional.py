93e247a8b87d839bfe8ed3d7783637ce1b02667f
s-vakoliuk <s.vakoliuk@gmail.com>
2022-02-23 21:14:18 +0200

    21 february 2022

--- additional.py
+++ additional.py
@@ -1,23 +1,21 @@
 # Створити функцію котра приймає число і повертає так, як в прикладі.
 # Наприклад: ввели 3275, а виведе "3000 + 200 + 70 +5"
 
-def bitNumber (number):
+def bit_number(number):
     number = str(number)
-    listOfNumbers = list(number)  # Переведення в список рядків
+    list_of_numbers = list(number)  # Переведення в список рядків
 
-    reverseListOfNumbers = listOfNumbers[::-1]  # Формуємо зворотній список чисел
+    reverse_list_of_numbers = list_of_numbers[::-1]  # Формуємо зворотній список чисел
 
     result = []
-    for index, value in enumerate(reverseListOfNumbers):
-        if index == 0:
-           value = value
-        else:
-            count = index
+    for index, value in enumerate(reverse_list_of_numbers):
+        count = index
+        if value !="0":
             value = value+("0"*count)
-        result.append(value)
+            result.append(value)
 
     result[::-1] = result
-    resultString = "+".join(result) # З'єднуємо елементи списку знаком "+"
-    return print("Результат рядка: ", resultString)
+    result_string = "+".join(result) # З'єднуємо елементи списку знаком "+"
+    return print("Результат рядка: ", result_string)
 
-bitNumber(3275)
+bit_number(300502700501)
